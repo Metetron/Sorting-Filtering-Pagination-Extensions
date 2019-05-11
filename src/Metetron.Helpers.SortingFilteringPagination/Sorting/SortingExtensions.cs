2a0fa582a413a6ccb976c7e0ea4f0739cdd6c074
Daniel Freydl <daniel.freydl@epcos.com>
2019-05-11 10:48:16 +0200

    add extensions for sorting

---
+++ src/Metetron.Helpers.SortingFilteringPagination/Sorting/SortingExtensions.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Metetron.Helpers.SortingFilteringPagination.QueryObjects;
+
+namespace Metetron.Helpers.SortingFilteringPagination.Sorting
+{
+    public static class SortingExtensions
+    {
+        public static IQueryable<T> ApplySorting<T>(this IQueryable<T> query, IDictionary<string, Expression<Func<T, object>>> columnMapping, IQueryObject queryObj)
+        {
+            if (string.IsNullOrWhiteSpace(queryObj.SortBy) || !columnMapping.ContainsKey(queryObj.SortBy))
+                return query;
+
+            if (queryObj.IsSortAscending)
+                return query.OrderBy(columnMapping[queryObj.SortBy]);
+
+            return query.OrderByDescending(columnMapping[queryObj.SortBy]);
+        }
+    }
+}
