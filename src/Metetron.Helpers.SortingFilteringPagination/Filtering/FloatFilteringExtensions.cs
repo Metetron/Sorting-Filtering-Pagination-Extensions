71c707251228eb067e5a34bf3bbba7e1b22184e1
Daniel Freydl <daniel.freydl@epcos.com>
2019-05-14 18:25:29 +0200

    add xml documentation

--- src/Metetron.Helpers.SortingFilteringPagination/Filtering/FloatFilteringExtensions.cs
+++ src/Metetron.Helpers.SortingFilteringPagination/Filtering/FloatFilteringExtensions.cs
@@ -7,26 +7,67 @@ namespace Metetron.Helpers.SortingFilteringPagination.Filtering
 {
     public static class FloatFilteringExtensions
     {
+        /// <summary>
+        /// Applies a 'greater than'- filter to the query
+        /// </summary>
+        /// <param name="query">The IQueryable to query</param>
+        /// <param name="propertyToFilter">The property to query</param>
+        /// <param name="constant">The value to compare against</param>
+        /// <typeparam name="T">The type of object to query</typeparam>
+        /// <returns>The filtered IQueryable</returns>
         public static IQueryable<T> ApplyGreaterThanFilter<T>(this IQueryable<T> query, Expression<Func<T, float>> propertyToFilter, float constant)
         {
             return query.Where(q => propertyToFilter.Compile().Invoke(q) > constant);
         }
 
+        /// <summary>
+        /// Applies a 'greater than or equal to'- filter to the query
+        /// </summary>
+        /// <param name="query">The IQueryable to query</param>
+        /// <param name="propertyToFilter">The property to query</param>
+        /// <param name="constant">The value to compare against</param>
+        /// <typeparam name="T">The type of object to query</typeparam>
+        /// <returns>The filtered IQueryable</returns>
         public static IQueryable<T> ApplyGreaterThanOrEqualToFilter<T>(this IQueryable<T> query, Expression<Func<T, float>> propertyToFilter, float constant)
         {
             return query.Where(q => propertyToFilter.Compile().Invoke(q) >= constant);
         }
 
+        /// <summary>
+        /// Applies a 'smaller than'- filter to the query
+        /// </summary>
+        /// <param name="query">The IQueryable to query</param>
+        /// <param name="propertyToFilter">The property to query</param>
+        /// <param name="constant">The value to compare against</param>
+        /// <typeparam name="T">The type of object to query</typeparam>
+        /// <returns>The filtered IQueryable</returns>
         public static IQueryable<T> ApplySmallerThanFilter<T>(this IQueryable<T> query, Expression<Func<T, float>> propertyToFilter, float constant)
         {
             return query.Where(q => propertyToFilter.Compile().Invoke(q) < constant);
         }
 
+        /// <summary>
+        /// Applies a 'smaller than or equal to'- filter to the query
+        /// </summary>
+        /// <param name="query">The IQueryable to query</param>
+        /// <param name="propertyToFilter">The property to query</param>
+        /// <param name="constant">The value to compare against</param>
+        /// <typeparam name="T">The type of object to query</typeparam>
+        /// <returns>The filtered IQueryable</returns>
         public static IQueryable<T> ApplySmallerThanOrEqualToFilter<T>(this IQueryable<T> query, Expression<Func<T, float>> propertyToFilter, float constant)
         {
             return query.Where(q => propertyToFilter.Compile().Invoke(q) <= constant);
         }
 
+        /// <summary>
+        /// Applies an 'equal to'- filter to the query
+        /// </summary>
+        /// <param name="query">The IQueryable to query</param>
+        /// <param name="propertyToFilter">The property to query</param>
+        /// <param name="constant">The value to compare against</param>
+        /// <param name="delta">The allowed deviation from zero</param>
+        /// <typeparam name="T">The type of object to query</typeparam>
+        /// <returns>The filtered IQueryable</returns>
         public static IQueryable<T> ApplyEqualToFilter<T>(this IQueryable<T> query, Expression<Func<T, float>> propertyToFilter, float constant, float delta = 1e-5f)
         {
             return query.Where(q => Abs(propertyToFilter.Compile().Invoke(q) - constant) <= delta);
--- src/Metetron.Helpers.SortingFilteringPagination/Filtering/IntegerFilteringExtensions.cs
+++ src/Metetron.Helpers.SortingFilteringPagination/Filtering/IntegerFilteringExtensions.cs
@@ -6,26 +6,66 @@ namespace Metetron.Helpers.SortingFilteringPagination.Filtering
 {
     public static class IntegerFilteringExtensions
     {
+        /// <summary>
+        /// Applies a 'greater than'- filter to the query
+        /// </summary>
+        /// <param name="query">The IQueryable to query</param>
+        /// <param name="propertyToFilter">The property to query</param>
+        /// <param name="constant">The value to compare against</param>
+        /// <typeparam name="T">The type of object to query</typeparam>
+        /// <returns>The filtered IQueryable</returns>
         public static IQueryable<T> ApplyGreaterThanFilter<T>(this IQueryable<T> query, Expression<Func<T, int>> propertyToFilter, int constant)
         {
             return query.Where(q => propertyToFilter.Compile().Invoke(q) > constant);
         }
 
+        /// <summary>
+        /// Applies a 'greater than or equal to'- filter to the query
+        /// </summary>
+        /// <param name="query">The IQueryable to query</param>
+        /// <param name="propertyToFilter">The property to query</param>
+        /// <param name="constant">The value to compare against</param>
+        /// <typeparam name="T">The type of object to query</typeparam>
+        /// <returns>The filtered IQueryable</returns>
         public static IQueryable<T> ApplyGreaterThanOrEqualToFilter<T>(this IQueryable<T> query, Expression<Func<T, int>> propertyToFilter, int constant)
         {
             return query.Where(q => propertyToFilter.Compile().Invoke(q) >= constant);
         }
 
+        /// <summary>
+        /// Applies a 'smaller than'- filter to the query
+        /// </summary>
+        /// <param name="query">The IQueryable to query</param>
+        /// <param name="propertyToFilter">The property to query</param>
+        /// <param name="constant">The value to compare against</param>
+        /// <typeparam name="T">The type of object to query</typeparam>
+        /// <returns>The filtered IQueryable</returns>
         public static IQueryable<T> ApplySmallerThanFilter<T>(this IQueryable<T> query, Expression<Func<T, int>> propertyToFilter, int constant)
         {
             return query.Where(q => propertyToFilter.Compile().Invoke(q) < constant);
         }
 
+        /// <summary>
+        /// Applies a 'smaller than or equal to'- filter to the query
+        /// </summary>
+        /// <param name="query">The IQueryable to query</param>
+        /// <param name="propertyToFilter">The property to query</param>
+        /// <param name="constant">The value to compare against</param>
+        /// <typeparam name="T">The type of object to query</typeparam>
+        /// <returns>The filtered IQueryable</returns>
         public static IQueryable<T> ApplySmallerThanOrEqualToFilter<T>(this IQueryable<T> query, Expression<Func<T, int>> propertyToFilter, int constant)
         {
             return query.Where(q => propertyToFilter.Compile().Invoke(q) <= constant);
         }
 
+        /// <summary>
+        /// Applies an 'equal to'- filter to the query
+        /// </summary>
+        /// <param name="query">The IQueryable to query</param>
+        /// <param name="propertyToFilter">The property to query</param>
+        /// <param name="constant">The value to compare against</param>
+        /// <typeparam name="T">The type of object to query</typeparam>
+        /// <returns>The filtered IQueryable</returns>
         public static IQueryable<T> ApplyEqualToFilter<T>(this IQueryable<T> query, Expression<Func<T, int>> propertyToFilter, int constant)
         {
             return query.Where(q => propertyToFilter.Compile().Invoke(q) == constant);
